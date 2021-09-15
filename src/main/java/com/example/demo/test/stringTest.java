e41bdd31fea998e19e5688217a0efaa334966855
王凯 <wangkai5@bonc.com.cn>
2021-09-15 11:37:12 +0800

    demo

--- src/main/java/com/example/demo/test/stringTest.java
+++ src/main/java/com/example/demo/test/stringTest.java
@@ -2,6 +2,8 @@ package com.example.demo.test;
 
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
@@ -9,6 +11,11 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.math.BigDecimal;
 import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class stringTest {
     public static void main(String[] args) {
@@ -19,6 +26,34 @@ public class stringTest {
 
         }
         System.out.println(i);
+
+        HashMap<String, String> map1 = new HashMap<String, String>() {{
+            put("provinceCode", "09");
+            put("id", "1");
+        }};
+        System.out.println(String.join("|", map1.values()));
+
+        List<Map<String, String>> resulList = new ArrayList<>();
+        HashMap<String, String> hashMap = new HashMap<>();
+        resulList.add(new HashMap<String, String>(){{
+            put("provinceCode", "09");
+            put("id", "1");}});
+        resulList.add(new HashMap<String, String>(){{
+            put("provinceCode", "09");
+            put("id", "2");}});
+//        resulList.add(new HashMap<String, String>(){{
+//            put("provinceCode", "19");
+//            put("id", "3");}});
+//        resulList.add(new HashMap<String, String>(){{
+//            put("provinceCode", "39");
+//            put("id", "4");}});
+        System.out.println(resulList.toString());
+        List<Map<String, String>> filterResultList = resulList.stream().filter(x -> (!StringUtils.isEmpty(x.get("provinceCode")) && !"09".equals(x.get("provinceCode")))).collect(Collectors.toList());
+        if (!CollectionUtils.isEmpty(filterResultList)) {
+            resulList = filterResultList;
+        }
+        System.out.println(resulList.toString());
+
 
 
     }
